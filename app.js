60970df6c14bcf73bde9387a8021433a29d94132
Ellie <eagyeman1234@gmail.com>
2023-11-22 12:06:44 -0500

    use the unshift method to add an element to the front of an array

--- app.js
+++ app.js
@@ -25,11 +25,27 @@
 // accessing the first movie in the array by accessing it by index 0. Arrays do not go start off at 1 they start off at 0
 
 
-// ADDING ELEMENTS TO AN ARRAY 
+
+
+
+// ADDING ELEMENTS TO AN ARRAY (to the end of the array )
+
+// let movies = ['Clueless', 'Avatar', 'The Bee Movie']
+
+// movies.push('Home')
+
+// console.log(movies)
+
+// the push method is available on all arrays it is built in. the push method adds the specified elements to the END of an array and then returns the NEW LENGTH of the array.
+
+
+
+//ADDING ELEMENTS TO AN ARRAY (to the front of the array )
 
 let movies = ['Clueless', 'Avatar', 'The Bee Movie']
 
-movies.push('Home')
+movies.unshift('Home')
 
 console.log(movies)
-// the push method is available on all arrays it is built in. the push method adds the specified elements to the end of an array and then returns the NEW LENGTH of the array.
+
+// the unshift method adds an element to the front of an array
