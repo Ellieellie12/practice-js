8e7e4bb03ea331cd73f885435132896ad9d70dae
Ellie <eagyeman1234@gmail.com>
2023-11-22 11:28:54 -0500

    check console.log for change in x

--- app.js
+++ app.js
@@ -4,5 +4,13 @@
 // x = 100 
 // console.log(x)
 
-const x = 25
+// const x = 25
+// console.log(x)
+// const stays the same (constant)
+
+let x = 2
+x = 5
+
 console.log(x)
+
+// you can reassign let 
