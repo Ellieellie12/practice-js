376e8f148207c5f3d88e047a9335745c6d55dfad
Ellie <eagyeman1234@gmail.com>
2023-11-28 11:01:18 -0500

    add notes for getPointsScored function

--- functions.js
+++ functions.js
@@ -9,3 +9,6 @@ function getPointsScored(elapsedTime) {
 }
 
 const points = getPointsScored(30)
+//invoking the function 
+// the return will be 75 because the elapsed time is 30 points
+//if the elapsed time was changed to 90 it would return 25 points
